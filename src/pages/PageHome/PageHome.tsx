6ba17ec4db03e77b2de0101ed4cc0f4f85f0803b
1538986328 <1538986328@qq.com>
2025-02-05 15:20:24 +0800

    feat: change wap css

--- src/pages/PageHome/PageHome.tsx
+++ src/pages/PageHome/PageHome.tsx
@@ -34,11 +34,11 @@ const ComBanner = (props: any) => {
       </div>
       <div className="btns">
         <div className="css-btn-click">
-          <span className="text family-proto-bold">PBTC</span>
+          <span className="text family-proto-bold size-24">PBTC</span>
           <i className="i-arrow"></i>
         </div>
         <div className="css-btn-click orange">
-          <span className="text family-proto-bold">PUSD</span>
+          <span className="text family-proto-bold size-24">PUSD</span>
           <i className="i-arrow"></i>
         </div>
       </div>
@@ -52,7 +52,7 @@ const ComBanner = (props: any) => {
 const ComTogether = (props: any) => {
   return (
     <div className="level-3 com-together">
-      <div className="title family-proto">
+      <div className="title family-proto size-54">
         Bitcoin x Dollars-Better together
       </div>
       <div className="context">
@@ -70,7 +70,7 @@ const ComTogether = (props: any) => {
             staking token (LST)
           </div>
           <div className="ctx-3 css-btn-click orange-white">
-            <span className="family-proto">pBTC</span>
+            <span className="family-proto size-24">pBTC</span>
             <i className="i-arrow"></i>
           </div>
         </div>
@@ -88,7 +88,7 @@ const ComTogether = (props: any) => {
             increase
           </div>
           <div className="ctx-3 css-btn-click orange-white">
-            <span className="family-proto">pUSD</span>
+            <span className="family-proto size-24">pUSD</span>
             <i className="i-arrow"></i>
           </div>
         </div>
@@ -99,48 +99,57 @@ const ComTogether = (props: any) => {
 const ComPusd = (props: any) => {
   return (
     <div className="level-3 com-pusd">
-      <div className="title family-proto">
+      <div className="title family-proto size-54">
         Stake Bitcoin to get BTC yields and Borrow stablecoin pUSD
       </div>
       <div className="times">
         <div className="time-item time-1">
           <div className="i-con-box"><i className="i-coin-t1"></i></div>
-          <p className="t-title family-proto">STAKE BITCOIN</p>
-          <p className="t-notice family-proto">
+          <p className="t-title family-proto size-24">STAKE BITCOIN</p>
+          <p className="t-notice family-proto size-12">
             Deposit supported assets like: WBTC, cbBTC, solvBTC, &amp; more
           </p>
+          {/* <div className="pos-wap">
+            <div className="jt pos1"></div>
+          </div> */}
         </div>
         <div className="time-item time-2">
           <div className="i-con-box"><i className="i-coin-t2"></i></div>
-          <p className="t-title family-proto">EARN BITCOIN</p>
-          <p className="t-notice family-proto">
+          <p className="t-title family-proto size-24">EARN BITCOIN</p>
+          <p className="t-notice family-proto size-12">
             Receive LST pBTC to earn from staking yields
           </p>
+          {/* <div className="pos-wap">
+            <div className="jt pos2"></div>
+          </div> */}
         </div>
         <div className="time-item time-3">
           <div className="i-con-box"><i className="i-coin-t3"></i></div>
-          <p className="t-title family-proto">BORROW DOLLARS</p>
-          <p className="t-notice family-proto">
+          <p className="t-title family-proto size-24">BORROW DOLLARS</p>
+          <p className="t-notice family-proto size-12">
             Borrow stablecoin pUSD to use across Defi Z
           </p>
+          {/* <div className="pos-wap">
+            <div className="jt pos3"></div>
+          </div> */}
         </div>
         <div className="time-4">
           <div className="time-item time-5">
-            <p className="t-title family-proto">Leverage</p>
-            <p className="t-notice family-proto">
+            <p className="t-title family-proto size-24">Leverage</p>
+            <p className="t-notice family-proto size-12">
               Increase your exposure to BTC, up to 11x
             </p>
           </div>
           <div className="time-item time-6">
-            <p className="t-title family-proto">EARN</p>
-            <p className="t-notice family-proto">
+            <p className="t-title family-proto size-24">EARN</p>
+            <p className="t-notice family-proto size-12">
               Stake pUSD and earn yields from protocol revenue, liquidation
               proceeds, and other sources.
             </p>
           </div>
           <div className="time-item time-7">
-            <p className="t-title family-proto">PAY</p>
-            <p className="t-notice family-proto">
+            <p className="t-title family-proto size-24">PAY</p>
+            <p className="t-notice family-proto size-12">
               Use pUSD as everyday money and buy yourself a nice little
               something
             </p>
